984d22de49eac9bb19fbac3fa1da912a8c804ba9
Sampath Sukesh Ravolaparthi <rsampaths16@gmail.com>
2017-12-17 13:53:24 +0530

    a two layer NN. bug found and fixed.

--- basic-neural-net.py
+++ basic-neural-net.py
@@ -8,13 +8,9 @@ def nonlin(x, deriv=False):
 
 #input data <- binary here
 X = np.array([
-    [0, 0, 0],
     [0, 0, 1],
-    [0, 1, 0],
     [0, 1, 1],
-    [1, 0, 0],
     [1, 0, 1],
-    [1, 1, 0],
     [1, 1, 1],
 ])
 
@@ -24,10 +20,6 @@ y = np.array([
     [1],
     [1],
     [0],
-    [1],
-    [0],
-    [0],
-    [1],
 ])
 
 #deterministic random numbers
@@ -35,37 +27,30 @@ np.random.seed(1)
 
 #synapses
 syn0 = 2*np.random.random((3, 8)) - 1
-syn1 = 2*np.random.random((8, 8)) - 1
-syn2 = 2*np.random.random((8, 1)) - 1
+syn1 = 2*np.random.random((8, 1)) - 1
 
 #training step
 epochs = 60000
-alpha = 0.05
+alpha = 1 #learing rate
 for j in range(epochs):
     #forward pass
     l0 = X
     l1 = nonlin(np.dot(l0, syn0))
     l2 = nonlin(np.dot(l1, syn1))
-    l3 = nonlin(np.dot(l2, syn2))
     
     # calculate error
-    l3_error = y - l3
-    print "Error:", str(np.mean(np.abs(l3_error)))
+    l2_error = y - l2
+    if (j%10000) == 0:
+        print "Error:", str(np.mean(np.abs(l2_error)))
 
     #backpropagation
-    l3_delta = l3_error * nonlin(l3, deriv=True)
-    l2_error = l3_delta.dot(syn2.T)
     l2_delta = l2_error * nonlin(l2, deriv=True)
     l1_error = l2_delta.dot(syn1.T)
-    l1_delta = l1_error = nonlin(l1, deriv=True)
+    l1_delta = l1_error * nonlin(l1, deriv=True)
 
     #update weights
-    syn2 += l2.T.dot(l3_delta)
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
+    syn1 += alpha * l1.T.dot(l2_delta)
+    syn0 += alpha * l0.T.dot(l1_delta)
 
 print "Output after training"
-print l3
-
-
-
+print l2
